835692f57f0a7de6882fa049582e825aee8be3bf
t.emidio.worko <t_emidio@hotmail.com>
2020-04-18 17:47:22 -0300

    Added 'New topics', Added shortcuts and fixed URL

--- Helloworld.py
+++ Helloworld.py
@@ -1,3 +1,8 @@
+#I use it to start python venv in Vscode without having to write it down.
+#python manage.py runserver - start the server
+#http://127.0.0.1:8000 -localhost
+#cd myproject
+
 print('hello world')
 
 for i in range(6):
