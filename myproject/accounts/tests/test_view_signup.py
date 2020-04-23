5f4f263c46f090be4e45647ebde4ee23a3f1d4ca
t.emidio.worko <t_emidio@hotmail.com>
2020-04-23 16:17:47 -0300

    improved the testing

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import resolve, reverse
 from django.test import TestCase
-from .views import signup
-from .forms import SignUpForm
+from ..views import signup
+from ..forms import SignUpForm
 
 # Create your tests here.
 class SignUpTests(TestCase):
